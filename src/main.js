379a99976666e58bd052f866936ad2c32aabab7d
tamaramunoz <tami.skt@gmail.com>
2020-01-05 12:29:52 -0300

    accediendo a los objetos

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const getNameMovieAsync = async (movieTitle) => {
+    try {
+        const resTitle = await fetch(`http://www.omdbapi.com/?t=${movieTitle}&apikey=8a5b424a`)
+        const title = await resTitle.json()
+        // const printTitle = Object.values(title)
+        console.log(title)
+        
+    } catch (error) {
+        console.log(error);
+    }
+   
+} 
+getNameMovieAsync("titanic")
