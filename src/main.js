e5dfeb8cbfa00b78771c40e1ed3a6a94e41e2e11
tamaramunoz <tami.skt@gmail.com>
2020-01-07 09:50:16 -0300

    fetch funcionando

--- src/main.js
+++ src/main.js
@@ -42,17 +42,17 @@ const selectingMovie = document.getElementById('filterTitle');
 selectingMovie.addEventListener('change', getTitleMovie);
 
 function getTitleMovie() {
-    const choosingMovie = document.querySelector('#filterTitle').value;
-    console.log(choosingMovie);
+  const choosingMovie = document.querySelector('#filterTitle').value;
+  console.log(choosingMovie);
 
-    fetch(`http://www.omdbapi.com/?t=${choosingMovie}&apikey=8a5b424a`)
-      .then(res => res.json())
-      .then(movieDB => {
-         console.log(movieDB)
-        const containerMovies = document.getElementById('container');
-        containerMovies.innerHTML = '';
-        containerMovies.innerHTML += getCard(movieDB);
-      })
+  fetch(`http://www.omdbapi.com/?t=${choosingMovie}&apikey=8a5b424a`)
+    .then(res => res.json())
+    .then(movieDB => {
+      console.log(movieDB)
+      const containerMovies = document.getElementById('container');
+      containerMovies.innerHTML = '';
+      containerMovies.innerHTML += getCard(movieDB);
+    })
     .catch(err => {
       console.log(err);
     })
