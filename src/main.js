12e34745d16d6541a1981f4cdcf7f3ab6cf0f203
reyesvi92@gmail.com <reyesvi92@gmail.com>
2020-01-07 22:50:35 -0300

    function button home and hide container when selecting movie

---
+++ src/main.js
@@ -0,0 +1,76 @@
+function getCard(movie) {
+  return `<div class="wrap">
+              <div class="card-wrap">
+            <div class="card">
+          <div class="front">
+           <h4>Title: ${movie.Title}</h4>
+           <img class="imagen" src="${movie.Poster}" alt="poster movie">
+          </div>
+         <div class="back"> 
+         <lo> 
+            <li>${movie.Plot}</li>
+            <li>Genre: ${movie.Genre}</li>
+            <li>Actors: ${movie.Actors} </li>  
+            <li>Rating: ${movie.imdbRating}</li>
+          </lo>
+         </div>
+       </div>
+       </div>
+   </div>`;
+}
+
+const getNameMovie = (movieTitle) => {
+  fetch(`http://www.omdbapi.com/?t=${movieTitle}&apikey=8a5b424a`)
+    .then(res => res.json())
+    .then(movie => {
+
+      const containerMovies = document.getElementById('container');
+      containerMovies.innerHTML += getCard(movie)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+const movieNominated = () => {
+  getNameMovie('Bombshell')
+  getNameMovie('The Irishman')
+  getNameMovie('Parasite')
+  getNameMovie('Marriage Story')
+  getNameMovie('1917')
+}
+movieNominated();
+
+const selectingMovie = document.getElementById('filterTitle');
+selectingMovie.addEventListener('change', getTitleMovie);
+
+function getTitleMovie() {
+  const choosingMovie = document.querySelector('#filterTitle').value;
+  console.log(choosingMovie);
+
+  fetch(`http://www.omdbapi.com/?t=${choosingMovie}&apikey=8a5b424a`)
+    .then(res => res.json())
+    .then(movieDB => {
+      console.log(movieDB)
+      const containerMovies = document.getElementById('container');
+      containerMovies.innerHTML = '';
+      containerMovies.innerHTML += getCard(movieDB);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
+// function for none content
+const btnFilter = document.getElementById('filterTitle');
+btnFilter.addEventListener("click", () => {
+  contenido.style.display = "none";
+})
+
+// function for return start
+const btnInicio = document.getElementById('inicio');
+btnInicio.addEventListener("click", () => {
+  contenido.style.display = "flex";
+  const containerMovies = document.getElementById('container');
+  containerMovies.innerHTML = '';
+  movieNominated();
+})
