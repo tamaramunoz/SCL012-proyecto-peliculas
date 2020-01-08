33053a6fa7b0d52a5b95d78102f6d7d9ac3fbc2d
tamaramunoz <tami.skt@gmail.com>
2020-01-08 13:30:09 -0300

    arreglando tarjetas

--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
 function getCard(movie) {
-  return `<div class="card-wrap">
-              <div class="card">
-                <div class="front">
-                  <h4>Title: ${movie.Title}</h4>
-                  <img class="imagen" src="${movie.Poster}" alt="poster movie">
+  return `<div class="card mb-3" style="max-width: 540px;"">
+              <div "row no-gutters">
+                <div "col-md-4">
+                  <img class="card-img" src="${movie.Poster}" alt="poster movie">
                 </div>
-                <div class="back"> 
-                <lo> 
-                  <li>${movie.Plot}</li>
-                  <li>Genre: ${movie.Genre}</li>
-                  <li>Actors: ${movie.Actors} </li>  
-                  <li>Rating: ${movie.imdbRating}</li>
-                </lo>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">${movie.Title}</h5>
+                    <p class="card-text">${movie.Plot}</p>
+                    <p class="card-text"><small class="text-muted">${movie.Genre}</small></p>
+                    <p class="card-text"><small class="text-muted">${movie.Actors}</small></p>
+                    <p class="card-text"><small class="text-muted">${movie.imdbRating}</small></p>
+                  </div>
                 </div>
               </div>
-            </div>
           </div>`;
 }
 
