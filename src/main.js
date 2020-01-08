f7fa5f26de2c68ecd26e6cce3ebc453870f97890
JocelynJofreAntoine <joce.jofreantoine@gmail.com>
2020-01-08 10:11:37 -0300

    avances html y css

---
+++ src/main.js
@@ -0,0 +1,59 @@
+function getCard(movie) {
+    return `<div class="wrap">
+              <div class="card-wrap">
+            <div class="card">
+          <div class="front">
+           <h4>Title: ${movie.Title}</h4>
+           <img class="imagen" src="${movie.Poster}" alt="poster movie">
+          </div>
+         <div class="back"> 
+         <lo> 
+            <li>${movie.Plot}</li>
+            <li>Genre: ${movie.Genre}</li>
+            <li>Actors: ${movie.Actors} </li>  
+            <li>Rating: ${movie.imdbRating}</li>
+          </lo>
+         </div>
+       </div>
+       </div>
+   </div>`;
+  }
+  
+  const getNameMovie = (movieTitle) => {
+    fetch(`http://www.omdbapi.com/?t=${movieTitle}&apikey=8a5b424a`)
+      .then(res => res.json())
+      .then(movie => {
+  
+        const containerMovies = document.getElementById('container');
+        containerMovies.innerHTML += getCard(movie)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+  getNameMovie('Bombshell')
+  getNameMovie('The Irishman')
+  getNameMovie('Parasite')
+  getNameMovie('Marriage Story')
+  getNameMovie('1917')
+  
+  
+  const selectingMovie = document.getElementById('filterTitle');
+  selectingMovie.addEventListener('change', getTitleMovie);
+  
+  function getTitleMovie() {
+      const choosingMovie = document.querySelector('#filterTitle').value;
+      console.log(choosingMovie);
+  
+      fetch(`http://www.omdbapi.com/?t=${choosingMovie}&apikey=8a5b424a`)
+        .then(res => res.json())
+        .then(movieDB => {
+           console.log(movieDB)
+          const containerMovies = document.getElementById('container');
+          containerMovies.innerHTML = '';
+          containerMovies.innerHTML += getCard(movieDB);
+        })
+      .catch(err => {
+        console.log(err);
+      })
+  }
